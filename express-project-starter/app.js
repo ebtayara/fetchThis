65d4845e42a916c25ac977d73b8d9f501b66848b
Eddy <edkim220@gmail.com>
2021-05-05 12:19:05 -0400

    worked on splash/styling

--- express-project-starter/app.js
+++ express-project-starter/app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const { restoreUser } = require('./auth.js');
 // const validator = require('');
 
 
@@ -37,7 +38,7 @@ app.use(
 
 // create Session table if it doesn't already exist
 store.sync();
-
+app.use(restoreUser);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
