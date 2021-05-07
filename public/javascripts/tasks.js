66101c32ce8be359c473454d7b4841d300f2d2bf
Ebrahim Tayara <ebrahim@tayara.net>
2021-05-07 09:23:53 -0700

    adjusted flex container

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -1,13 +1,15 @@
 const task = require("../../db/models/task")
 
 //check to see if checkbox is checked
-const checkbox = document.getElementById('task.id')
-console.log(checkbox.checked)
+// const checkbox = document.getElementById('task.id')
+// console.log(checkbox.checked)
 
-if(checked) {
-    //route to completed task in summary page?
-}
+// if(checked) {
+//     //route to completed task in summary page?
+// }
 
 // textarea.addEventListener('click', e => {
 //     textarea.hide();
 // });
+
+
