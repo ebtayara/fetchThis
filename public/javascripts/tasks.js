41fc8bcc889b81f27238adf684cfb1713f4aaa22
Ebrahim Tayara <ebrahim@tayara.net>
2021-05-07 10:52:57 -0700

    can't figure this out

--- public/javascripts/tasks.js
+++ public/javascripts/tasks.js
@@ -1,7 +1,8 @@
 const task = require("../../db/models/task")
 
 const addButton = document.getElementById('addButton');
-// const text = document.getElementById('textarea');
+const text = document.getElementById('textarea');
+const divToAddToo = document.querySelector('#tasksFlex')
 
 //check to see if checkbox is checked
 // const checkbox = document.getElementById('task.id')
@@ -15,15 +16,19 @@ const addButton = document.getElementById('addButton');
 //     textarea.hide();
 // });
 
-addButton.addEventListener('click', e => {
-    const button = e.target.id === 'addButton';
-    const { task } = task.name
-    if(button) {
-        text = task.name //something like that?
-    }
-});
+// addButton.addEventListener('click', e => {
+//     const button = e.target.id === 'addButton';
+//     const { task } = task.name
+//     if(button) {
+//         text = task.name //something like that?
+//     }
+// });
 
-addButton.addEventListener('click', async (e) => {
-    const text = document.getElementById('textarea');
-    window.location.href = (`/tasks/${text.value}`)
+// addButton.addEventListener('click', async (e) => {
+//     const text = document.getElementById('textarea');
+//     window.location.href = (`/tasks/${text.value}`)
+// });
+
+addButton.addEventListener('click', e => {
+    divToAddToo.innerHTML = text.innerHTML //also tried text.value
 });
